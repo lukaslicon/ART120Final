ac08f27c1f56fa9e36973f17fb7cab3530ed682c
Lukas Licon <lukas@licons.com>
2023-06-04 16:07:20 -0700

    working

--- cinematics/intro.js
+++ cinematics/intro.js
@@ -3,12 +3,17 @@ class intro extends Phaser.Scene {
         super('intro');
     }
     create() {
-        let image = this.add.sprite(960, 538, 'introScreen');
+        let image = this.add.sprite(960, 540, 'introScreen');
         image.alpha = 0;
         this.fadeInthenOut(image, 2000, 2000);
 
         let introText = "As the last vibrations of the portal die away, you find yourself standing amidst a crumbled, apocalypse-ravaged world. The quiet whispers of the mountains echo around you, their familiar yet alien outlines resembling a life once known, now bathed in the uneasy stillness of decay. The scent of salt air intermingles with the charred remnants of a civilization, hinting at the nearby ocean, a stark reminder of a time and place akin to UC Santa Cruz, yet profoundly different.";
-        let text = this.add.text(960, 538, introText, { font: "24px Arial", fill: "#ffffff", align: "center" }).setAlpha(0);
+        let text = this.add.text(960, 538, introText, { 
+            font: "42px Arial", 
+            fill: "#ffffff", 
+            align: "center",
+            wordWrap: { width: 800 } // wrap words that exceed this width
+        }).setOrigin(0.5).setAlpha(0); // set origin to center
         this.fadeIn(text, 2000, 5500);
     }
     fadeInthenOut(target, time1, time2){
