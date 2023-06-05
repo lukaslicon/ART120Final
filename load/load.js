f888e2a04b8f31213ae241a67bb9f7302809eb94
Lukas Licon <lukas@licons.com>
2023-06-04 18:06:03 -0700

    new files
    Co-authored-by: Akhrxr <Akhrxr@users.noreply.github.com>

--- load/load.js
+++ load/load.js
@@ -11,7 +11,6 @@ class load extends Phaser.Scene {
         this.load.spritesheet('OutroGif', 'spritesheet.png', { frameWidth: 1920, frameHeight: 1082 });
 
         //npcScreen
-        
         this.load.image('player', 'npcScreen/PixelArtCharacter.png');
         this.load.image('NPC', 'npcScreen/PixelArtNPC.png');
 
@@ -24,12 +23,39 @@ class load extends Phaser.Scene {
         this.load.image("boarder", "game1/boarder.png");
         this.load.image("side", "game1/sideBoarder.png");
         this.load.image("reset", "game1/reset.png");
+        this.load.image("topHouse", "game1/topHouse.png");
+        this.load.image("bottomHouse", "game1/bottomHouse.png");
+        this.load.image("leftHouse", "game1/leftHouse.png");
+        this.load.image("rightHouse", "game1/rightHouse.png");
 
         //game2
+        this.load.image('coin', 'game2/slug.png');
+        this.load.image('bg','game2/background.png');
+        this.load.image('sh1','game2/shfull.png');
+        this.load.image('sh2','game2/sh2.png');
+        this.load.image('sh3','game2/sh3.png');
+        this.load.image('sh4','game2/shno.png');
+        this.load.image('coinb','game2/badslug.png');
+        this.load.audio('dmg','game2/damage.wav');
+        this.load.audio('catch','game2/catch.wav');
+
+        //game3
+        this.load.image('turbo', 'game3/turbo.png');
+        this.load.image('bg','game3/background.png');
+        this.load.image('obs','game3/KoiDown.png');
+        this.load.image('obs2','game3/KoiUp.png');
+        this.load.image('obs3','game3/KoiUpR.png');
+        this.load.image('waste','game3/waste.png');
+        this.load.audio('dmg','game3/damage.wav');
+        this.load.audio('catch','game3/catch.wav');
+        this.load.image('sh1','game3/shfull.png')
+        this.load.image('sh2','game3/sh2.png')
+        this.load.image('sh3','game3/sh3.png')
+        this.load.image('sh4','game3/shno.png')
     }
     create()
     {
-        this.scene.start('outro');
+        this.scene.start('intro');
     }
 }
 
