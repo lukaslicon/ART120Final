93f9d67af1b56bf1e9929c9bf72efe7f94d1be44
Lukas Licon <lukas@licons.com>
2023-06-08 08:21:24 -0700

    sizing

--- load/main.js
+++ load/main.js
@@ -1,16 +1,25 @@
 
 let config = {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 1920,
-    height: 1080,
+    type:Phaser.AUTO,
+    pixelArt: true,
+    zoom: 1,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080,
+    },
     physics:{
         default: 'arcade',
         arcade: {
+            gravity: {
+                x: 0,
+                y: 0
+            },
             debug: false,
             debugShowVelocity: false
-    }
-},
+        }
+    },
 scene: [load, intro, title, npcScreen, MiniGame1, MiniGame2, MiniGame3, Fail, Fail2, outro, UI, Homeless],
 title: "Mini Game Prototype",
 };
